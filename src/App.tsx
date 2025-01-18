c36d4d6b536a9223a10ecde0e877d2a39b164d8d
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-01-17 22:44:02 -0500

    created Editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Editor from "./components/Editor";
+
 function App() {
   return (
     <>
       <div className="flex h-full w-full items-center justify-center overflow-hidden">
-        <div className="h-full w-6/12 relative">Editor</div>
+        <Editor />
       </div>
     </>
   );
---
+++ src/components/Editor.tsx
@@ -0,0 +1,19 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+const Editor = () => {
+  return (
+    <div className="h-full w-6/12 relative">
+      <CodeMirror
+        minHeight="100vh"
+        value="Hello World"
+        onChange={() => {}}
+        theme="dark"
+        extensions={[javascript({ jsx: true })]}
+        style={{ fontSize: "30px" }}
+      />
+    </div>
+  );
+};
+
+export default Editor;
